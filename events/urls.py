0a6ca36288112770408404f9ac792032df3fd0e8
unknown <brayanopiyo18@gmail.com>
2022-07-18 11:37:32 +0300

    My first commit

---
+++ events/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('<int:year>/<str:month>/', views.home, name='home'),
+    path('events', views.all_events, name='list-events'),
+    path('add_venue', views.add_venue, name='add-venue'),
+    path('list_venues', views.list_venues, name='list-venues'),
+    path('show_venue/<venue_id>',views.show_venue ,name='show-venue'),
+    path('search_venue',views.search_venue ,name='search-venue'),
+    path('update_venue/<venue_id>',views.update_venue ,name='update-venue'),
+    path('add_event',views.add_event ,name='add-event'),
+]
